41ec0693569df1deb66a2c732dfcc22febd908b8
Daniil Tiganov <tiganov@ualberta.ca>
2019-05-01 14:07:14 -0600

    attemp to fix hook

--- com.ibm.wala.cast.swift/swift-wala-translator/lib/WALASupport/WALAInstance.cpp
+++ com.ibm.wala.cast.swift/swift-wala-translator/lib/WALASupport/WALAInstance.cpp
@@ -32,10 +32,13 @@ struct Observer : public FrontendObserver {
     Invocation.setRuntimeResourcePath(LibPath.str());
   }
 
-  // TODO: it looks like this infrastructure has substantially changed.
-  // We need to figure out how to hook into the compiler in Swift 5.0.
-  void performedSILGeneration(SILModule &Module) override {
-    Instance->analyzeSILModule(Module);
+  static std::deque<PostSILGenInputs>
+  generateSILModules(CompilerInvocation &Invocation, CompilerInstance &CompilerInstance) override {
+    if (auto Module = CompilerInstance.takeSILModule())
+    {
+      Instance->analyzeSILModule(Module);
+    }
+    return FrontendObserver::generateSILModules(Invocation, CompilerInstance);
   }
 };
 
