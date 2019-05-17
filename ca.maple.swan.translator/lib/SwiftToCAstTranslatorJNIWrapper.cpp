23350593f69ac3deda9f01866f92030e8899f4d4
Daniil Tiganov <tiganov@ualberta.ca>
2019-05-17 14:54:02 -0600

    changed naming to fit new structure

--- ca.maple.swan.translator/lib/SwiftToCAstTranslatorJNIWrapper.cpp
+++ ca.maple.swan.translator/lib/SwiftToCAstTranslatorJNIWrapper.cpp
@@ -9,18 +9,14 @@
 
 #include <jni.h>
 #include <memory>
-#include <swift-wala/WALASupport/WALAInstance.h>
+#include <WALAInstance.h>
+#include "SwiftToCAstTranslatorJNIWrapper.h"
 
 using namespace swift_wala;
 
-extern "C" {
-
-JNIEXPORT jobject JNICALL
-Java_com_ibm_wala_cast_swift_SwiftToCAstTranslator_translateToCAstNodes(JNIEnv *env, jobject obj)
-	{
-	  WALAInstance Instance(env, obj);
-	  Instance.analyze();
-	  // Make your Java Array here
-	  return Instance.getCAstNodes();
-	}
+JNIEXPORT void JNICALL
+Java_ca_maple_swan_analysis_translator_SwiftToCAstTranslator_translateToCAstNodes(JNIEnv *env, jobject obj)
+{
+    WALAInstance Instance(env, obj);
+    Instance.analyze();
 }
