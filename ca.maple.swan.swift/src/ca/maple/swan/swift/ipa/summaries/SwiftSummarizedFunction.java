e98feb8f6011a25b87e3f036ea1b253264ae0f5d
Daniil Tiganov <tiganov@ualberta.ca>
2019-07-03 17:01:36 -0600

    Added license header

--- ca.maple.swan.swift/src/ca/maple/swan/swift/ipa/summaries/SwiftSummarizedFunction.java
+++ ca.maple.swan.swift/src/ca/maple/swan/swift/ipa/summaries/SwiftSummarizedFunction.java
@@ -1,3 +1,16 @@
+//===--- SwiftSummarizedFunction.java ------------------------------------===//
+//
+// This source file is part of the SWAN open source project
+//
+// Copyright (c) 2019 Maple @ University of Alberta
+// All rights reserved. This program and the accompanying materials (unless
+// otherwise specified by a license inside of the accompanying material)
+// are made available under the terms of the Eclipse Public License v2.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v20.html
+//
+//===---------------------------------------------------------------------===//
+
 package ca.maple.swan.swift.ipa.summaries;
 
 import ca.maple.swan.swift.cfg.SwiftInducedCFG;
