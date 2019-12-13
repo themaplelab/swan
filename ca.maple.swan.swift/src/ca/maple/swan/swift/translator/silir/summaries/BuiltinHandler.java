a5b862364f1dcf922215844bbb0c6869b051daa4
Daniil Tiganov <tiganov@ualberta.ca>
2019-12-13 14:19:45 -0600

    Added many built in functions as summarize todo

--- ca.maple.swan.swift/src/ca/maple/swan/swift/translator/silir/summaries/BuiltinHandler.java
+++ ca.maple.swan.swift/src/ca/maple/swan/swift/translator/silir/summaries/BuiltinHandler.java
@@ -35,6 +35,40 @@ public class BuiltinHandler {
         // TODO: Handle all Array, Set, and Dictionary container type functions.
         // TODO: os_log needs to have all values of given array sunk.
 
+
+        // TODO:
+        /*
+        func $() `Swift.Array.append(__owned A) -> ()`(v0 : $*String, v1 : $*Array<String>)
+        func $Array<Int> `Swift.Array.init() -> Swift.Array<A>`(v0 : $@thin Array<Int>.Type)
+        func $Array<Int> `Swift.Array.init(repeating: A, count: Swift.Int) -> Swift.Array<A>`(v0 : $*Int, v1 : $Builtin.IntLiteral, v2 : $@thin Array<Int>.Type)
+        func $Array<Int> `Swift.Array.init<A where A == A1.Element, A1: Swift.Sequence>(A1) -> Swift.Array<A>`(v0 : $*ClosedRange<Int>, v1 : $@thin Array<Int>.Type)
+        func $Bool `(extension in Swift):Swift.Collection.isEmpty.getter : Swift.Bool`(v0 : $*Array<Int>)
+        func $Int `Swift.Array.count.getter : Swift.Int`(v0 : $Array<Int>)
+        func $Int `Swift.Array.capacity.getter : Swift.Int`(v0 : $Array<String>)
+        func $() `(extension in Swift):Swift.Collection.first.getter : Swift.Optional<A.Element>`(v0 : $*Optional<String>, v1 : $*Array<String>)
+        func $() `(extension in Swift):Swift.BidirectionalCollection.last.getter : Swift.Optional<A.Element>`(v0 : $*Optional<String>, v1 : $*Array<String>)
+        func $() `(extension in Swift):Swift.MutableCollection.subscript.getter : <A where A1: Swift.RangeExpression, A.Index == A1.Bound>(A1) -> A.SubSequence`(v0 : $*ArraySlice<Int>, v1 : $*ClosedRange<Int>, v2 : $*Array<Int>)
+        func $Int `Swift.Array.endIndex.getter : Swift.Int`(v0 : $Array<String>)
+        func $ArraySlice<String> `Swift.Array.subscript.getter : (Swift.Range<Swift.Int>) -> Swift.ArraySlice<A>`(v0 : $Range<Int>, v1 : $Array<String>)
+        func $() `(extension in Swift):Swift.Collection< where A.Element: Swift.Equatable>.firstIndex(of: A.Element) -> Swift.Optional<A.Index>`(v0 : $*Optional<Int>, v1 : $*String, v2 : $*Array<String>)
+        func $() `Swift._diagnoseUnexpectedNilOptional(_filenameStart: Builtin.RawPointer, _filenameLength: Builtin.Word, _filenameIsASCII: Builtin.Int1, _line: Builtin.Word, _isImplicitUnwrap: Builtin.Int1) -> ()`(v0 : $Builtin.RawPointer, v1 : $Builtin.Word, v2 : $Builtin.Int1, v3 : $Builtin.Word, v4 : $Builtin.Int1)
+        func $() `Swift.Array.insert(_: __owned A, at: Swift.Int) -> ()`(v0 : $*Int, v1 : $Builtin.IntLiteral, v2 : ${ var Array<Int> })
+        func $() `(extension in Swift):Swift.RangeReplaceableCollection.insert<A where A1: Swift.Collection, A.Element == A1.Element>(contentsOf: __owned A1, at: A.Index) -> ()`(v0 : $*ClosedRange<Int>, v1 : $*Int, v2 : ${ var Array<Int> })
+        func $() `Swift.repeatElement<A>(_: A, count: Swift.Int) -> Swift.Repeated<A>`(v0 : $*Repeated<Int>, v1 : $*Int, v2 : $Builtin.IntLiteral)
+        func $() `(extension in Swift):Swift.RangeReplaceableCollection.replaceSubrange<A, B where A1: Swift.Collection, B1: Swift.RangeExpression, A.Element == A1.Element, A.Index == B1.Bound>(_: B1, with: __owned A1) -> ()`(v0 : $*ClosedRange<Int>, v1 : $*Repeated<Int>, v2 : ${ var Array<Int> })
+        func $() `Swift.Array.reserveCapacity(Swift.Int) -> ()`(v0 : $Builtin.IntLiteral, v1 : ${ var Array<Int> })
+        func $() `Swift.Array.append<A where A == A1.Element, A1: Swift.Sequence>(contentsOf: __owned A1) -> ()`(v0 : $*ClosedRange<Int>, v1 : ${ var Array<Int> })
+        func $() `Swift.Array.remove(at: Swift.Int) -> A`(v0 : $*Int, v1 : $Builtin.IntLiteral, v2 : ${ var Array<Int> })
+        func $() `(extension in Swift):Swift.RangeReplaceableCollection.removeFirst() -> A.Element`(v0 : $*Int, v1 : ${ var Array<Int> })
+        func $() `(extension in Swift):Swift.RangeReplaceableCollection.removeFirst(Swift.Int) -> ()`(v0 : $Builtin.IntLiteral, v1 : ${ var Array<Int> })
+        func $() `(extension in Swift):Swift.RangeReplaceableCollection< where A: Swift.BidirectionalCollection>.removeLast() -> A.Swift.Sequence.Element`(v0 : $*Int, v1 : ${ var Array<Int> })
+        func $() `(extension in Swift):Swift.RangeReplaceableCollection< where A: Swift.BidirectionalCollection>.removeLast(Swift.Int) -> ()`(v0 : $Builtin.IntLiteral, v1 : ${ var Array<Int> })
+        func $() `(extension in Swift):Swift.RangeReplaceableCollection.removeSubrange<A where A1: Swift.RangeExpression, A.Index == A1.Bound>(A1) -> ()`(v0 : $*ClosedRange<Int>, v1 : ${ var Array<Int> })
+        func $() `(extension in Swift):Swift.RangeReplaceableCollection< where A: Swift.BidirectionalCollection>.popLast() -> Swift.Optional<A.Swift.Sequence.Element>`(v0 : $*Optional<Int>, v1 : ${ var Array<Int> })
+        func $Bool `default argument 0 of Swift.Array.removeAll(keepingCapacity: Swift.Bool) -> ()`()
+        func $() `Swift.Array.removeAll(keepingCapacity: Swift.Bool) -> ()`(v0 : $Bool, v1 : ${ var Array<Int> })
+        */
+
         switch(funcName) {
 
             case "Swift.Array.append(__owned A) -> ()": {
