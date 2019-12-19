60ed196b2963c779e82025ff2b895f0f0ea8d236
Daniil Tiganov <tiganov@ualberta.ca>
2019-12-19 11:20:12 -0600

    Added some test files (more to come)

---
+++ ca.maple.swan.swift.test/testFiles/taint/flowDroidFigure2.swift
@@ -0,0 +1,34 @@
+class A {
+    var g : B;
+
+    init() {
+        self.g = B();
+    }
+}
+
+class B {
+    var f : String;
+
+    init() {
+        self.f = "I'm not tainted (yet)"
+    }
+}
+
+func source() -> String {
+    return "I'm bad";
+}
+
+func sink(sunk: String) {
+    print(sunk);
+}
+
+func foo(z : A) {
+    let x = z.g;
+    let w = source();
+    x.f = w;
+}
+
+let a = A();
+let b = a.g;
+foo(z: a);
+sink(sunk: b.f);
