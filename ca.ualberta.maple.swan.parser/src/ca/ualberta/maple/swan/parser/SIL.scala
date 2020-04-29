725668af2e82590ae862260354aaee7eae0c47e1
Daniil Tiganov <tiganov@ualberta.ca>
2020-04-29 14:50:17 -0600

    Merged Parser -> SILParser, finished SIL.scala

--- ca.ualberta.maple.swan.parser/src/ca/ualberta/maple/swan/parser/SIL.scala
+++ ca.ualberta.maple.swan.parser/src/ca/ualberta/maple/swan/parser/SIL.scala
@@ -2,12 +2,14 @@ package ca.ualberta.maple.swan.parser
 
 import java.nio.file.Path
 
+import ca.ualberta.maple.swan
+
 class Module(val functions: Array[Function]) {
 
   object Parse {
     @throws[Error]
     def parsePath(silPath: Path): Module = {
-      val parser = new SILParser(silPath) // TODO: auxiliary can't call superclass constructor
+      val parser = new SILParser(silPath)
       parser.parseModule()
     }
 
@@ -26,7 +28,6 @@ class Block(val identifier: String, val arguments: Array[Argument],
             val operatorDefs: Array[OperatorDef], val terminatorDef: TerminatorDef) {
 
   def ==(that: Block): Boolean = {
-    // Not sure if this actually works. String comparison is not .equals(), either.
     (identifier, arguments, operatorDefs, terminatorDef) == (that.identifier, that.arguments, that.operatorDefs, that.terminatorDef)
   }
 }
@@ -35,22 +36,33 @@ class OperatorDef(val result: Option[Result], val operator: Operator, val source
 
 class TerminatorDef(val terminator: Terminator, val sourceInfo: Option[SourceInfo])
 
-// TODO: enum InstructionDef
+sealed trait InstructionDef {
+  val instruction : Instruction
+}
+object InstructionDef {
+  case class operator(val operatorDef: OperatorDef) extends InstructionDef {
+    val instruction: Instruction = swan.parser.Instruction.operator(operatorDef.operator)
+  }
+  case class terminator(val terminatorDef: TerminatorDef) extends InstructionDef {
+    val instruction: Instruction = swan.parser.Instruction.terminator(terminatorDef.terminator)
+  }
+}
 
 sealed trait Operator
+object Operator {
   case class allocStack(`type`: Type, attributes: Array[DebugAttribute]) extends Operator
   case class apply(
-    nothrow: Boolean, value: String,
-    substitutions: Array[Type], arguments: Array[String], `type`: Type
-  ) extends Operator
+                    nothrow: Boolean, value: String,
+                    substitutions: Array[Type], arguments: Array[String], `type`: Type
+                  ) extends Operator
   case class beginAccess(
-    access: Access, enforcement: Enforcement, noNestedConflict: Boolean, builtin: Boolean,
-    operand: Operand
-  ) extends Operator
+                          access: Access, enforcement: Enforcement, noNestedConflict: Boolean, builtin: Boolean,
+                          operand: Operand
+                        ) extends Operator
   case class beginApply(
-    nothrow: Boolean, value: String,
-    substitutions: Array[Type], arguments: Array[String], `type`: Type
-  ) extends Operator
+                         nothrow: Boolean, value: String,
+                         substitutions: Array[Type], arguments: Array[String], `type`: Type
+                       ) extends Operator
   case class beginBorrow(operand: Operand) extends Operator
   case class builtin(name: String, operands: Array[Operand], `type`: Type) extends Operator
   case class condFail(operand: Operand, message: String) extends Operator
@@ -76,9 +88,9 @@ sealed trait Operator
   case class markDependence(operand: Operand, on: Operand) extends Operator
   case class metatype(`type`: Type) extends Operator
   case class partialApply(
-    calleeGuaranteed: Boolean, onStack: Boolean, value: String,
-    substitutions: Array[Type], arguments: Array[String], `type`: Type
-  ) extends Operator
+                           calleeGuaranteed: Boolean, onStack: Boolean, value: String,
+                           substitutions: Array[Type], arguments: Array[String], `type`: Type
+                         ) extends Operator
   case class pointerToAddress(operand: Operand, strict: Boolean, `type`: Type) extends Operator
   case class releaseValue(operand: Operand) extends Operator
   case class retainValue(operand: Operand) extends Operator
@@ -93,48 +105,62 @@ sealed trait Operator
   case class thinToThickFunction(operand: Operand, `type`: Type) extends Operator
   case class tuple(elements: TupleElements) extends Operator
   case class tupleExtract(operand: Operand, declRef: Int) extends Operator
-  case class unknown_operator(name: String) extends Operator
+  case class unknown(name: String) extends Operator
   case class witnessMethod(archeType: Type, declRef: DeclRef, declType: Type, `type`: Type) extends Operator
+}
 
 sealed trait Terminator
+object Terminator {
   case class br(label: String, operands: Array[Operand]) extends Terminator
   case class condBr(cond: String,
                     trueLabel: String, trueOperands: Array[Operand],
                     falseLabel: String, falseOperands: Array[Operand]) extends Terminator
   case class `return`(operand: Operand) extends Terminator
   case class switchEnum(operand: Operand, cases: Array[Case]) extends Terminator
-  case class unknown_terminator(name: String) extends Terminator
+  case class unknown(name: String) extends Terminator
   case class unreachable() extends Terminator
+}
 
 sealed trait Instruction
-  case class operator(op: Operator)
-  case class terminator(t: Terminator)
+object Instruction {
+  case class operator(op: Operator) extends Instruction
+  case class terminator(t: Terminator) extends  Instruction
+}
 
 sealed trait Access
+object Access {
   case class deinit() extends Access
-  case class init_access() extends Access
+  case class init() extends Access
   case class modify() extends Access
   case class read() extends Access
+}
 
 class Argument(val valueName: String, val `type`: Type)
 
 sealed trait Case
+object Case {
   case class `case`(declRef: DeclRef, result: String) extends Case
   case class default(result: String) extends Case
+}
 
 sealed trait Convention
+object Convention {
   case class c() extends Convention
   case class method() extends Convention
-  case class thin_convention() extends Convention
-  case class witnessMethod_convention(`type`: Type) extends Convention
+  case class thin() extends Convention
+  case class witnessMethod(`type`: Type) extends Convention
+}
 
 sealed trait DebugAttribute
+object DebugAttribute {
   case class argno(index: Int) extends DebugAttribute
   case class name(name: String) extends DebugAttribute
   case class let() extends DebugAttribute
   case class `var`() extends DebugAttribute
+}
 
 sealed trait DeclKind
+object DeclKind {
   case class allocator() extends DeclKind
   case class deallocator() extends DeclKind
   case class destroyer() extends DeclKind
@@ -145,21 +171,27 @@ sealed trait DeclKind
   case class ivarDestroyer() extends DeclKind
   case class ivarInitializer() extends DeclKind
   case class setter() extends DeclKind
+}
 
 class DeclRef(val name: Array[String], val kind: Option[DeclKind], val level: Option[Int])
 
 sealed trait Encoding
+object Encoding {
   case class objcSelector() extends Encoding
   case class utf8() extends Encoding
   case class utf16() extends Encoding
+}
 
 sealed trait Enforcement
+object Enforcement {
   case class dynamic() extends Enforcement
   case class static() extends Enforcement
-  case class unknown_enforcement() extends Enforcement
+  case class unknown() extends Enforcement
   case class safe() extends Enforcement
+}
 
 sealed trait FunctionAttribute
+object FunctionAttribute {
   case class alwaysInline() extends FunctionAttribute
   case class differentiable(spec: String) extends FunctionAttribute
   case class dynamicallyReplacable() extends FunctionAttribute
@@ -170,11 +202,15 @@ sealed trait FunctionAttribute
   case class thunk() extends FunctionAttribute
   case class transparent() extends FunctionAttribute
   case class noncanonical(attr: NoncanonicalFunctionAttribute) extends FunctionAttribute
+}
 
 sealed trait NoncanonicalFunctionAttribute
+object NoncanonicalFunctionAttribute {
   case class ownershipSSA() extends NoncanonicalFunctionAttribute
+}
 
 sealed trait Linkage
+object Linkage {
   case class hidden() extends Linkage
   case class hiddenExternal() extends Linkage
   case class `private`() extends Linkage
@@ -184,6 +220,7 @@ sealed trait Linkage
   case class publicNonABI() extends Linkage
   case class shared() extends Linkage
   case class sharedExternal() extends Linkage
+}
 
 class Loc(val path: String, val line: Int, val column: Int)
 
@@ -194,13 +231,34 @@ class Result(val valueNames: Array[String])
 class SourceInfo(val scopeRef: Option[Int], val loc: Option[Loc])
 
 sealed trait TupleElements
+object TupleElements {
   case class labeled(`type`: Type, values: Array[String])
   case class unlabeled(operands: Array[Operand])
+}
 
 sealed trait Type
-// TODO
+object Type {
+  case class addressType(`type`: Type) extends Type
+  case class attributedType(attributes: Array[TypeAttribute], `type`: Type) extends Type
+  case class coroutineTokenType() extends Type
+  case class functionType(parameters: Array[Type], result: Type) extends Type
+  case class genericType(parameters: Array[String], requirements: Array[TypeRequirement], `type`: Type) extends Type
+  case class namedType(name: String) extends Type
+  case class selectType(`type`: Type, name: String) extends Type
+  case class selfType() extends Type
+  case class specializedType(`type`: Type, arguments: Array[Type]) extends Type
+  case class tupleType(parameters: Array[Type]) extends Type
+  case class withOwnership(attribute: TypeAttribute, `type`: Type) extends Type
+
+  @throws[Error]
+  def parse(silString: String): Type = {
+    val parser = new SILParser(silString)
+    parser.parseType()
+  }
+}
 
 sealed trait TypeAttribute
+object TypeAttribute {
   case class calleeGuaranteed() extends TypeAttribute
   case class convention(convention: Convention) extends TypeAttribute
   case class guaranteed() extends TypeAttribute
@@ -214,16 +272,24 @@ sealed trait TypeAttribute
   case class thin() extends TypeAttribute
   case class yieldOnce() extends TypeAttribute
   case class yields() extends TypeAttribute
+}
 
 sealed trait TypeRequirement
+object TypeRequirement {
   case class conformance(lhs: Type, rhs: Type) extends TypeRequirement
   case class equality(lhs: Type, rhs: Type) extends TypeRequirement
+}
 
 sealed trait LoadOwnership
+object LoadOwnership{
   case class copy() extends LoadOwnership
   case class take() extends LoadOwnership
-  case class load_trivial() extends LoadOwnership
+  case class trivial() extends LoadOwnership
+}
 
 sealed trait StoreOwnership
+object StoreOwnership {
   case class init() extends StoreOwnership
-  case class store_trivial() extends StoreOwnership
+  case class trivial() extends StoreOwnership
+}
+
