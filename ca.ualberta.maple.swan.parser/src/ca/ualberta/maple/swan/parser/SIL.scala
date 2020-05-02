47f3715cf9c16bc965f6c697757c805002097e17
Karim Ali <karim.ali@ualberta.ca>
2020-05-01 18:46:55 -0600

    translate the Swift SILParser to our Scala codebase next step is to translate the methods parseType and parseInstructionBody (this one is big!)

--- ca.ualberta.maple.swan.parser/src/ca/ualberta/maple/swan/parser/SIL.scala
+++ ca.ualberta.maple.swan.parser/src/ca/ualberta/maple/swan/parser/SIL.scala
@@ -32,7 +32,7 @@ class Module(val functions: Array[Function]) {
 }
 
 class Function(val linkage: Linkage, val attributes: Array[FunctionAttribute],
-               val name: String, val `type`: Type, val blocks: Array[Block])
+               val name: String, val tpe: Type, val blocks: Array[Block])
 
 class Block(val identifier: String, val arguments: Array[Argument],
             val operatorDefs: Array[OperatorDef], val terminatorDef: TerminatorDef) {
@@ -51,19 +51,19 @@ sealed trait InstructionDef {
 }
 object InstructionDef {
   case class operator(val operatorDef: OperatorDef) extends InstructionDef {
-    val instruction: Instruction = swan.parser.Instruction.operator(operatorDef.operator)
+    val instruction: Instruction = Instruction.operator(operatorDef.operator)
   }
   case class terminator(val terminatorDef: TerminatorDef) extends InstructionDef {
-    val instruction: Instruction = swan.parser.Instruction.terminator(terminatorDef.terminator)
+    val instruction: Instruction = Instruction.terminator(terminatorDef.terminator)
   }
 }
 
 sealed trait Operator
 object Operator {
-  case class allocStack(`type`: Type, attributes: Array[DebugAttribute]) extends Operator
+  case class allocStack(tpe: Type, attributes: Array[DebugAttribute]) extends Operator
   case class apply(
                     nothrow: Boolean, value: String,
-                    substitutions: Array[Type], arguments: Array[String], `type`: Type
+                    substitutions: Array[Type], arguments: Array[String], tpe: Type
                   ) extends Operator
   case class beginAccess(
                           access: Access, enforcement: Enforcement, noNestedConflict: Boolean, builtin: Boolean,
@@ -71,13 +71,13 @@ object Operator {
                         ) extends Operator
   case class beginApply(
                          nothrow: Boolean, value: String,
-                         substitutions: Array[Type], arguments: Array[String], `type`: Type
+                         substitutions: Array[Type], arguments: Array[String], tpe: Type
                        ) extends Operator
   case class beginBorrow(operand: Operand) extends Operator
-  case class builtin(name: String, operands: Array[Operand], `type`: Type) extends Operator
+  case class builtin(name: String, operands: Array[Operand], tpe: Type) extends Operator
   case class condFail(operand: Operand, message: String) extends Operator
-  case class convertEscapeToNoescape(notGuaranteed: Boolean, escaped: Boolean, operand: Operand, `type`: Type) extends Operator
-  case class convertFunction(operand: Operand, withoutActuallyEscaping: Boolean, `type`: Type) extends Operator
+  case class convertEscapeToNoescape(notGuaranteed: Boolean, escaped: Boolean, operand: Operand, tpe: Type) extends Operator
+  case class convertFunction(operand: Operand, withoutActuallyEscaping: Boolean, tpe: Type) extends Operator
   case class copyAddr(take: Boolean, value: String, initialization: Boolean, operand: Operand) extends Operator
   case class copyValue(operand: Operand) extends Operator
   case class deallocStack(operand: Operand) extends Operator
@@ -88,35 +88,35 @@ object Operator {
   case class endAccess(abort: Boolean, operand: Operand) extends Operator
   case class endApply(value: String) extends Operator
   case class endBorrow(operand: Operand) extends Operator
-  case class `enum`(`type`: Type, declRef: DeclRef, operand: Option[Operand]) extends Operator
-  case class floatLiteral(`type`: Type, value: String) extends Operator
-  case class functionRef(name: String, `type`: Type) extends Operator
-  case class globalAddr(name: String, `type`: Type) extends Operator
+  case class enum(tpe: Type, declRef: DeclRef, operand: Option[Operand]) extends Operator
+  case class floatLiteral(tpe: Type, value: String) extends Operator
+  case class functionRef(name: String, tpe: Type) extends Operator
+  case class globalAddr(name: String, tpe: Type) extends Operator
   case class indexAddr(addr: Operand, index: Operand) extends Operator
-  case class integerLiteral(`type`: Type, value: Int) extends Operator
+  case class integerLiteral(tpe: Type, value: Int) extends Operator
   case class load(kind: Option[LoadOwnership], operand: Operand) extends Operator
   case class markDependence(operand: Operand, on: Operand) extends Operator
-  case class metatype(`type`: Type) extends Operator
+  case class metatype(tpe: Type) extends Operator
   case class partialApply(
                            calleeGuaranteed: Boolean, onStack: Boolean, value: String,
-                           substitutions: Array[Type], arguments: Array[String], `type`: Type
+                           substitutions: Array[Type], arguments: Array[String], tpe: Type
                          ) extends Operator
-  case class pointerToAddress(operand: Operand, strict: Boolean, `type`: Type) extends Operator
+  case class pointerToAddress(operand: Operand, strict: Boolean, tpe: Type) extends Operator
   case class releaseValue(operand: Operand) extends Operator
   case class retainValue(operand: Operand) extends Operator
-  case class selectEnum(operand: Operand, cases: Array[Case], `type`: Type) extends Operator
+  case class selectEnum(operand: Operand, cases: Array[Case], tpe: Type) extends Operator
   case class store(value: String, kind: Option[StoreOwnership], operand: Operand) extends Operator
   case class stringLiteral(encoding: Encoding, value: String) extends Operator
   case class strongRelease(operand: Operand) extends Operator
   case class strongRetain(operand: Operand) extends Operator
-  case class `struct`(`type`: Type, operands: Array[Operand]) extends Operator
+  case class struct(tpe: Type, operands: Array[Operand]) extends Operator
   case class structElementAddr(operand: Operand, declRef: DeclRef) extends Operator
   case class structExtract(operand: Operand, declRef: DeclRef) extends Operator
-  case class thinToThickFunction(operand: Operand, `type`: Type) extends Operator
+  case class thinToThickFunction(operand: Operand, tpe: Type) extends Operator
   case class tuple(elements: TupleElements) extends Operator
   case class tupleExtract(operand: Operand, declRef: Int) extends Operator
   case class unknown(name: String) extends Operator
-  case class witnessMethod(archeType: Type, declRef: DeclRef, declType: Type, `type`: Type) extends Operator
+  case class witnessMethod(archeType: Type, declRef: DeclRef, declType: Type, tpe: Type) extends Operator
 }
 
 sealed trait Terminator
@@ -125,7 +125,7 @@ object Terminator {
   case class condBr(cond: String,
                     trueLabel: String, trueOperands: Array[Operand],
                     falseLabel: String, falseOperands: Array[Operand]) extends Terminator
-  case class `return`(operand: Operand) extends Terminator
+  case class ret(operand: Operand) extends Terminator
   case class switchEnum(operand: Operand, cases: Array[Case]) extends Terminator
   case class unknown(name: String) extends Terminator
   case class unreachable() extends Terminator
@@ -139,17 +139,17 @@ object Instruction {
 
 sealed trait Access
 object Access {
-  case class deinit() extends Access
-  case class init() extends Access
-  case class modify() extends Access
-  case class read() extends Access
+  case object deinit extends Access
+  case object init extends Access
+  case object modify extends Access
+  case object read extends Access
 }
 
-class Argument(val valueName: String, val `type`: Type)
+class Argument(val valueName: String, val tpe: Type)
 
 sealed trait Case
 object Case {
-  case class `case`(declRef: DeclRef, result: String) extends Case
+  case class cs(declRef: DeclRef, result: String) extends Case
   case class default(result: String) extends Case
 }
 
@@ -158,7 +158,7 @@ object Convention {
   case class c() extends Convention
   case class method() extends Convention
   case class thin() extends Convention
-  case class witnessMethod(`type`: Type) extends Convention
+  case class witnessMethod(tpe: Type) extends Convention
 }
 
 sealed trait DebugAttribute
@@ -166,7 +166,7 @@ object DebugAttribute {
   case class argno(index: Int) extends DebugAttribute
   case class name(name: String) extends DebugAttribute
   case class let() extends DebugAttribute
-  case class `var`() extends DebugAttribute
+  case class variable() extends DebugAttribute
 }
 
 sealed trait DeclKind
@@ -202,39 +202,39 @@ object Enforcement {
 
 sealed trait FunctionAttribute
 object FunctionAttribute {
-  case class alwaysInline() extends FunctionAttribute
+  case object alwaysInline extends FunctionAttribute
   case class differentiable(spec: String) extends FunctionAttribute
-  case class dynamicallyReplacable() extends FunctionAttribute
-  case class noInline() extends FunctionAttribute
-  case class readonly() extends FunctionAttribute
+  case object dynamicallyReplacable extends FunctionAttribute
+  case object noInline extends FunctionAttribute
+  case object readonly extends FunctionAttribute
   case class semantics(value: String) extends FunctionAttribute
-  case class serialized() extends FunctionAttribute
-  case class thunk() extends FunctionAttribute
-  case class transparent() extends FunctionAttribute
+  case object serialized extends FunctionAttribute
+  case object thunk extends FunctionAttribute
+  case object transparent extends FunctionAttribute
   case class noncanonical(attr: NoncanonicalFunctionAttribute) extends FunctionAttribute
 }
 
 sealed trait NoncanonicalFunctionAttribute
 object NoncanonicalFunctionAttribute {
-  case class ownershipSSA() extends NoncanonicalFunctionAttribute
+  case object ownershipSSA extends NoncanonicalFunctionAttribute
 }
 
 sealed trait Linkage
 object Linkage {
-  case class hidden() extends Linkage
-  case class hiddenExternal() extends Linkage
-  case class `private`() extends Linkage
-  case class privateExternal() extends Linkage
-  case class public() extends Linkage
-  case class publicExternal() extends Linkage
-  case class publicNonABI() extends Linkage
-  case class shared() extends Linkage
-  case class sharedExternal() extends Linkage
+  case object hidden extends Linkage
+  case object hiddenExternal extends Linkage
+  case object priv extends Linkage
+  case object privateExternal extends Linkage
+  case object public extends Linkage
+  case object publicExternal extends Linkage
+  case object publicNonABI extends Linkage
+  case object shared extends Linkage
+  case object sharedExternal extends Linkage
 }
 
 class Loc(val path: String, val line: Int, val column: Int)
 
-class Operand(val value: String, val `type`: Type)
+class Operand(val value: String, val tpe: Type)
 
 class Result(val valueNames: Array[String])
 
@@ -242,23 +242,23 @@ class SourceInfo(val scopeRef: Option[Int], val loc: Option[Loc])
 
 sealed trait TupleElements
 object TupleElements {
-  case class labeled(`type`: Type, values: Array[String])
+  case class labeled(tpe: Type, values: Array[String])
   case class unlabeled(operands: Array[Operand])
 }
 
 sealed trait Type
 object Type {
-  case class addressType(`type`: Type) extends Type
-  case class attributedType(attributes: Array[TypeAttribute], `type`: Type) extends Type
+  case class addressType(tpe: Type) extends Type
+  case class attributedType(attributes: Array[TypeAttribute], tpe: Type) extends Type
   case class coroutineTokenType() extends Type
   case class functionType(parameters: Array[Type], result: Type) extends Type
-  case class genericType(parameters: Array[String], requirements: Array[TypeRequirement], `type`: Type) extends Type
+  case class genericType(parameters: Array[String], requirements: Array[TypeRequirement], tpe: Type) extends Type
   case class namedType(name: String) extends Type
-  case class selectType(`type`: Type, name: String) extends Type
+  case class selectType(tpe: Type, name: String) extends Type
   case class selfType() extends Type
-  case class specializedType(`type`: Type, arguments: Array[Type]) extends Type
+  case class specializedType(tpe: Type, arguments: Array[Type]) extends Type
   case class tupleType(parameters: Array[Type]) extends Type
-  case class withOwnership(attribute: TypeAttribute, `type`: Type) extends Type
+  case class withOwnership(attribute: TypeAttribute, tpe: Type) extends Type
 
   @throws[Error]
   def parse(silString: String): Type = {
--- ca.ualberta.maple.swan.parser/src/ca/ualberta/maple/swan/parser/SILParser.scala
+++ ca.ualberta.maple.swan.parser/src/ca/ualberta/maple/swan/parser/SILParser.scala
@@ -17,6 +17,7 @@ import java.util
 import ca.ualberta.maple.swan.utils.ExceptionReporter
 
 import scala.collection.mutable.ArrayBuffer
+import scala.util.control.Breaks
 
 class SILParser {
 
@@ -119,7 +120,7 @@ class SILParser {
   }
 
   @throws[Error]
-  protected def parseNullOrMany[T](pre: String, sep: String, suf: String, parseOne: () => T): Option[Array[T]] = {
+  protected def parseNilOrMany[T](pre: String, sep: String = "", suf: String = "", parseOne: () => T): Option[Array[T]] = {
     if (!peek(pre)) {
       return None
     }
@@ -129,7 +130,7 @@ class SILParser {
   @throws[Error]
   protected def parseMany[T](pre: String, sep: String, suf: String, parseOne: () => T): Array[T] = {
     take(pre)
-    var result = new ArrayBuffer[T]
+    val result = new ArrayBuffer[T]
     if (!peek(suf)) {
       var break = false
       while (!break) {
@@ -150,7 +151,7 @@ class SILParser {
 
   // ***** Error reporting *****
 
-  protected def parseError(message: String, at: Option[Int]): Error = {
+  protected def parseError(message: String, at: Option[Int] = None[Int]): Error = {
     val position = if (at.isDefined) at.get else cursor
     val newlines = chars.take(position).filter(_ == '\n')
     val line = newlines.length + 1
@@ -169,10 +170,10 @@ class SILParser {
     }
 
     override def getMessage: String = {
-      if (line == null) {
+      if (line.isEmpty) {
         return path + ": " + message
       }
-      if (column == null) {
+      if (column.isEmpty) {
         return path + ":" + line + ": " + message
       }
       path + ":" + line + ":" + column + ": " + message
@@ -181,16 +182,286 @@ class SILParser {
 
   /********** THIS CODE IS ORIGINALLY PART OF "SILParser" *******************/
 
-  // TODO
-
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#syntax
   @throws[Error]
   def parseModule(): Module = {
-    null
+    var functions = Array[Function]()
+    var done = false
+    while(!done) {
+      // TODO(#8): Parse sections of SIL printouts that don't start with "sil @".
+      // Meanwhile, skip those sections since we don't have a representation for them yet.
+      // Concretely: if the current line begins with "sil @", try to parse a Function.
+      // Otherwise, skip to the end of line and repeat.
+      if(peek("sil ")) {
+        val function = try parseFunction()
+        functions :+= function
+      } else {
+        Breaks.breakable {
+          if(skip(_ != "\n")) Breaks.break
+        }
+        done = true
+      }
+    }
+    new Module(functions)
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#functions
+  @throws[Error]
+  def parseFunction(): Function = {
+    take("sil")
+    val linkage = try parseLinkage()
+    val attributes = { try parseNilOrMany("[", parseOne = try parseFunctionAttribute) }.getOrElse(Array.empty[FunctionAttribute])
+    val name = try parseGlobalName()
+    try take(":")
+    val tpe = try parseType()
+    val blocks = { try parseNilOrMany("{", "", "}", try parseBlock) }.getOrElse(Array.empty[Block])
+    new Function(linkage, attributes, name, tpe, blocks)
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#basic-blocks
+  @throws[Error]
+  def parseBlock(): Block = {
+    val identifier = try parseIdentifier()
+    val arguments = { try parseNilOrMany("(", ",", ")", try parseArgument) }.getOrElse(Array.empty[Argument])
+    try take(":")
+    val (operatorDefs, terminatorDef) = try parseInstructionDefs()
+    new Block(identifier, arguments, operatorDefs, terminatorDef)
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#basic-blocks
+  @throws[Error]
+  def parseInstructionDefs(): (Array[OperatorDef], TerminatorDef) = {
+    var operatorDefs = Array[OperatorDef]()
+    var done = false
+    var termDef: TerminatorDef = null
+    while(!done){
+      try parseInstructionDef() match {
+        case InstructionDef.operator(operatorDef) => operatorDefs :+= operatorDef
+        case InstructionDef.terminator(terminatorDef) => return (operatorDefs, terminatorDef)
+      }
+      if(peek("bb") || peek("}")) {
+        if(operatorDefs.lastOption.nonEmpty) {
+          operatorDefs.last.operator match {
+            case Operator.unknown(instructionName) => { done = true; termDef = new TerminatorDef(Terminator.unknown(instructionName), None) }
+            case _ => throw parseError("block is missing a terminator")
+          }
+        }
+      }
+    }
+    (operatorDefs, termDef)
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#basic-blocks
+  @throws[Error]
+  def parseInstructionDef(): InstructionDef = {
+    val result = try parseResult()
+    val body = parseInstruction()
+    val sourceInfo = try parseSourceInfo()
+    body match {
+      case Instruction.operator(op) => InstructionDef.operator(new OperatorDef(result, op, sourceInfo))
+      case Instruction.terminator(terminator) => {
+        if(result.nonEmpty) throw parseError("terminator instruction shouldn't have any results")
+        InstructionDef.terminator(new TerminatorDef(terminator, sourceInfo))
+      }
+    }
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#instruction-set
+  @throws[Error]
+  def parseInstruction(): Instruction = {
+    val instructionName = take(x => x.isLetter || x == "_")
+    try {
+      null // TODO: Swift: parseInstructionBody(instructionName)
+    } catch {
+      // Try to recover to a point where resuming the parsing is sensible
+      // by skipping until the end of this line. This is only a heuristic:
+      // I don't think that the SIL specification guarantees that.
+      case _ => {
+        skip(_ != "\n" )
+        Instruction.operator(Operator.unknown(instructionName))
+      }
+    }
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#functions
+  @throws[Error]
+  def parseGlobalName(): String = {
+    val start = position
+    if(skip("@")) {
+      // TODO(#14): Make name parsing more thorough.
+      val name = take(x => x == "$" || x.isLetterOrDigit || x == "_" )
+      if(!name.isEmpty) {
+        return name
+      }
+    }
+    throw parseError("function name expected", Some(start))
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#values-and-operands
+  @throws[Error]
+  def parseIdentifier(): String = {
+    if(peek("\"")) {
+      s"\"${try parseString()}\""
+    } else {
+      val start = position
+      // TODO(#14): Make name parsing more thorough.
+      val identifier = take(x => x.isLetterOrDigit || x == "_")
+      if (!identifier.isEmpty) return identifier
+      throw parseError("identifier expected", Some(start))
+    }
+  }
+
+  @throws[Error]
+  def parseInt(): Int = {
+    // TODO(#26): Make number parsing more thorough.
+    val start = position
+    val radix = if(skip("0x")) 16 else 10
+    val s = take(x => x == "-" || x == "+" || Character.digit(x, 16) != -1)
+    val value = try { Integer.parseInt(s, radix) } catch { case _ => throw parseError("integer literal expected", Some(start))}
+    value
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#linkage
+  @throws[Error]
+  def parseLinkage(): Linkage = {
+    // The order in here is a bit relaxed because longer words need to come
+    // before the shorter ones to parse correctly.
+    if(skip("hidden_external")) Linkage.hiddenExternal
+    if(skip("hidden")) Linkage.hidden
+    if(skip("private_external")) Linkage.privateExternal
+    if(skip("private")) Linkage.priv
+    if(skip("public_external")) Linkage.publicExternal
+    if(skip("non_abi")) Linkage.publicNonABI
+    if(skip("public")) Linkage.public
+    if(skip("shared_external")) Linkage.sharedExternal
+    if(skip("shared")) Linkage.shared
+    Linkage.public
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#debug-information
+  @throws[Error]
+  def parseLoc(): Option[Loc] = {
+    if(!skip("loc")) None
+    val path = try parseString()
+    try take(":")
+    val line = try parseInt()
+    try take(":")
+    val column = try parseInt()
+    Some(new Loc(path, line, column))
+  }
+
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#basic-blocks
+  @throws[Error]
+  def parseResult(): Option[Result] = {
+    if(peek("%")) {
+      val valueName = try parseValueName()
+      try take("=")
+      Some(new Result(Array(valueName)))
+    } else if(peek("(")) {
+      val valueNames = try parseMany("(", ",", ")", try parseValueName)
+      try take("=")
+      Some(new Result(valueNames))
+    } else {
+      None
+    }
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#debug-information
+  @throws[Error]
+  def parseScopeRef(): Option[Int] = {
+    if(!skip("scope")) None
+    Some(try parseInt())
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#basic-blocks
+  @throws[Error]
+  def parseSourceInfo(): Option[SourceInfo] = {
+    // NB: The SIL docs say that scope refs precede locations, but this is
+    //     not true once you look at the compiler outputs or its source code.
+    if(!skip(",")) None
+    val loc = try parseLoc()
+    // NB: No skipping if we failed to parse the location.
+    val scopeRef = if(loc.isEmpty || skip(",")) try parseScopeRef() else None
+    // We've skipped the comma, so failing to parse any of those two
+    // components is an error.
+    if(scopeRef.isEmpty && loc.isEmpty) throw parseError("Failed to parse source info")
+    Some(new SourceInfo(scopeRef, loc))
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#basic-blocks
+  @throws[Error]
+  def parseArgument(): Argument = {
+    val valueName = try parseValueName()
+    try take(":")
+    val tpe = try parseType()
+    new Argument(valueName, tpe)
+  }
+
+  // Reverse-engineered from -emit-sil
+  @throws[Error]
+  def parseFunctionAttribute(): FunctionAttribute = {
+    @throws[Error]
+    def parseDifferentiable(): FunctionAttribute = {
+      try take("[differentiable")
+      val spec = try take(_ != "]" )
+      try take("]")
+      FunctionAttribute.differentiable(spec)
+    }
+
+    @throws[Error]
+    def parseSemantics(): FunctionAttribute = {
+      try take("[_semantics")
+      val value = try parseString()
+      try take("]")
+      FunctionAttribute.semantics(value)
+    }
+
+    if(skip("[always_inline]")) FunctionAttribute.alwaysInline
+    if(peek("[differentiable")) try parseDifferentiable()
+    if(skip("[dynamically_replacable]")) FunctionAttribute.dynamicallyReplacable
+    if(skip("[noinline]")) FunctionAttribute.noInline
+    if(skip("[ossa]")) FunctionAttribute.noncanonical(NoncanonicalFunctionAttribute.ownershipSSA)
+    if(skip("[readonly]")) FunctionAttribute.readonly
+    if(peek("[_semantics")) try parseSemantics()
+    if(skip("[serialized]")) FunctionAttribute.serialized
+    if(skip("[thunk]")) FunctionAttribute.thunk
+    if(skip("[transparent]")) FunctionAttribute.transparent
+    throw parseError("unknown function attribute")
+  }
+
+  @throws[Error]
+  def parseString(): String = {
+    // TODO(#24): Parse string literals with control characters.
+    try take("\"")
+    val s = take(_ != "\"")
+    try take("\"")
+    s
   }
 
   @throws[Error]
   def parseType(): Type = {
-    null
+    null // TODO: still needs conversion from the Swift codebase
   }
 
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#values-and-operands
+  @throws[Error]
+  def parseValue(): String = {
+    if(peek("%")) {
+      try parseValueName()
+    } else if(skip("undef")) {
+      "undef"
+    } else {
+      throw parseError("value expected")
+    }
+  }
+
+  // https://github.com/apple/swift/blob/master/docs/SIL.rst#values-and-operands
+  @throws[Error]
+  def parseValueName(): String = {
+    val start = position
+    if(!skip("%")) throw parseError("value expected", Some(start))
+    val identifier = try parseIdentifier()
+    "%" + identifier
+  }
 }
