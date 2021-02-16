0f3587571f1d72f13e0b6153b9f74b76cf85b52f
Daniil Tiganov <tiganov@ualberta.ca>
2021-02-16 14:37:45 +0200

    Add all methods to SWANCallGraph

--- ca.ualberta.maple.swan.spds/src/scala/ca/ualberta/maple/swan/spds/SWANCallGraph.scala
+++ ca.ualberta.maple.swan.spds/src/scala/ca/ualberta/maple/swan/spds/SWANCallGraph.scala
@@ -10,29 +10,21 @@
 
 package ca.ualberta.maple.swan.spds
 
-import java.util
-import java.util.Set
-
-import boomerang.scene.{CallGraph, Method}
+import boomerang.scene.CallGraph
 import ca.ualberta.maple.swan.ir.{CanFunction, CanModule, Constants}
 import com.google.common.collect.Sets
-
-import scala.util.control.Breaks.{break, breakable}
 
 // TODO: iOS lifecycle
 class SWANCallGraph(val module: CanModule) extends CallGraph {
 
   private val methods = Sets.newHashSet[SWANMethod]
 
-  // TODO: Add all methods
-  breakable {
-    module.functions.foreach(f => {
-      if (f.name == Constants.fakeMain) {
-        this.getEntryPoints.add(makeMethod(f))
-        break()
-      }
-    })
-  }
+  module.functions.foreach(f => {
+    val m = makeMethod(f)
+    if (f.name == Constants.fakeMain) {
+      this.getEntryPoints.add(m)
+    }
+  })
 
   def makeMethod(f: CanFunction): SWANMethod = {
     val m = new SWANMethod(f)
