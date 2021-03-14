a466eddc86984a3eb3184c4f436327ecf5062d6a
Daniil Tiganov <tiganov@ualberta.ca>
2021-03-15 00:35:00 +0200

    More energy API queries

--- jvm/ca.ualberta.maple.swan.client/src/ca/ualberta/maple/swan/client/Main.scala
+++ jvm/ca.ualberta.maple.swan.client/src/ca/ualberta/maple/swan/client/Main.scala
@@ -12,7 +12,7 @@ package ca.ualberta.maple.swan.client
 
 import java.io.File
 
-import ca.ualberta.maple.swan.ir.{CanFunction, Operator, Position}
+import ca.ualberta.maple.swan.ir.{CanFunction, Literal, Operator, Position}
 import ca.ualberta.maple.swan.ir.canonical.SWIRLPass
 import ca.ualberta.maple.swan.ir.raw.SWIRLGen
 import ca.ualberta.maple.swan.parser.SILParser
@@ -24,52 +24,108 @@ import scala.collection.mutable.ArrayBuffer
 
 object Main {
 
-  def genQueries(): ArrayBuffer[CanFunction => (Boolean, String, Option[Position])] = {
-    val qs = ArrayBuffer.empty[CanFunction => (Boolean, String, Option[Position])]
-    qs.append((c: CanFunction) => {
-      val msg = "inefficient CLLocationManager parameter"
-      var global = false
-      var desiredAccuracy = false
-      var position: Option[Position] = None
-      c.blocks.foreach(b => {
-        b.operators.foreach(opdef => {
-          opdef.operator match {
-            case Operator.builtinRef(_, name) => {
-              if (name == "#CLLocationManager.desiredAccuracy!setter.foreign") {
-                desiredAccuracy = true
-                position = opdef.position
-              }
+  class QueryResults(val found: Boolean, val msg: String, val pos: Option[Position])
+
+  val desiredAccuracyValues: Array[String] = Array(
+    "kCLLocationAccuracyHundredMeters",
+    "kCLLocationAccuracyBest",
+    "kCLLocationAccuracyKilometer",
+    "kCLLocationAccuracyBestForNavigation"
+  )
+
+  def desiredAccuracy(c: CanFunction, value: String): QueryResults = {
+    val msg = "desiredAccuracy setting: " + value
+    var global = false
+    var desiredAccuracy = false
+    var position: Option[Position] = None
+    c.blocks.foreach(b => {
+      b.operators.foreach(opdef => {
+        opdef.operator match {
+          case Operator.builtinRef(_, name) => {
+            if (name == "#CLLocationManager.desiredAccuracy!setter.foreign") {
+              desiredAccuracy = true
+              position = opdef.position
             }
-            case Operator.singletonRead(_, _, field) => {
-              if (field == "kCLLocationAccuracyHundredMeters") {
-                global = true
-              }
-            }
-            case _ =>
           }
-        })
+          case Operator.singletonRead(_, _, field) => {
+            if (field == "") {
+              global = true
+            }
+          }
+          case _ =>
+        }
       })
-      (global && desiredAccuracy, msg, position)
     })
-    qs
+    new QueryResults(global && desiredAccuracy, msg, position)
   }
 
-  def printQueryResult(msg: String, pos: Option[Position]): Unit = {
-    val sb = new StringBuilder
-    sb.append("\nDetected ")
-    sb.append(msg)
-    if (pos.nonEmpty) {
-      val p = pos.get
-      sb.append(" at\n  ")
-      sb.append(p.path)
-      sb.append(":")
-      sb.append(p.line)
-      sb.append(":")
-      sb.append(p.col)
-    } else {
-      sb.append(" (no source available)")
+  def distanceFilter(c: CanFunction): QueryResults = {
+    var float: Float = -1
+    var distanceFilter = false
+    var position: Option[Position] = None
+    c.blocks.foreach(b => {
+      b.operators.foreach(opdef => {
+        opdef.operator match {
+          case Operator.builtinRef(_, name) => {
+            if (name == "#CLLocationManager.distanceFilter!setter.foreign") {
+              distanceFilter = true
+              position = opdef.position
+            }
+          }
+          case Operator.literal(_, literal) => {
+            literal match {
+              case Literal.float(value) => {
+                float = value
+              }
+              case _ =>
+            }
+          }
+          case _ =>
+        }
+      })
+    })
+    val msg = "distanceFilter setting: " + float.toString
+    new QueryResults(float > 0 && distanceFilter, msg, position)
+  }
+
+  def startUpdatingLocation(c: CanFunction): QueryResults = {
+    val msg = "startUpdatingLocation method"
+    var found = false
+    var position: Option[Position] = None
+    c.blocks.foreach(b => {
+      b.operators.foreach(opdef => {
+        opdef.operator match {
+          case Operator.builtinRef(_, name) => {
+            if (name == "#CLLocationManager.startUpdatingLocation!foreign") {
+              found = true
+              position = opdef.position
+            }
+          }
+          case _ =>
+        }
+      })
+    })
+    new QueryResults(found, msg, position)
+  }
+
+  def printQueryResult(queryResults: QueryResults): Unit = {
+    if (queryResults.found) {
+      val sb = new StringBuilder
+      sb.append("\nDetected ")
+      sb.append(queryResults.msg)
+      if (queryResults.pos.nonEmpty) {
+        val p = queryResults.pos.get
+        sb.append(" at\n  ")
+        sb.append(p.path)
+        sb.append(":")
+        sb.append(p.line)
+        sb.append(":")
+        sb.append(p.col)
+      } else {
+        sb.append(" (no source available)")
+      }
+      System.out.println(sb.toString())
     }
-    System.out.println(sb.toString())
   }
 
   def main(args: Array[String]): Unit = {
@@ -82,8 +138,6 @@ object Main {
       throw new RuntimeException("Given path does not exist")
     }
 
-    val queries = genQueries()
-
     val silFiles = swanDir.listFiles((_: File, name: String) => name.endsWith(".sil"))
     for (sil <- silFiles) {
       val silParser = new SILParser(sil.toPath)
@@ -92,12 +146,11 @@ object Main {
       val canSwirlModule = SWIRLPass.runPasses(swirlModule)
 
       canSwirlModule.functions.foreach(f => {
-        queries.foreach(query => {
-          val results = query(f)
-          if (results._1) {
-            printQueryResult(results._2, results._3)
-          }
+        desiredAccuracyValues.foreach(v => {
+          printQueryResult(desiredAccuracy(f, v))
         })
+        printQueryResult(startUpdatingLocation(f))
+        printQueryResult(distanceFilter(f))
       })
     }
   }
