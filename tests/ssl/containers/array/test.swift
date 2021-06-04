ef186f6b8946ef701df96d3db7713fd003d8a0e0
Daniil Tiganov <tiganov@ualberta.ca>
2021-06-04 08:44:10 +0300

    Began adding container test section

---
+++ tests/ssl/containers/array/test.swift
@@ -0,0 +1,36 @@
+// https://developer.apple.com/documentation/swift/array
+
+func source() -> String {
+    return "I'm bad";
+}
+
+func sink(sunk: String) {
+    print(sunk);
+}
+
+// ------- Creating -------
+
+func test_init1() {
+  // Slightly incorrect path
+  let src = source();
+  let arr = [src]; //!testing!source
+  sink(sunk : arr[0]); //!testing!sink
+}
+
+func test_init2() {
+  let src = source();
+  let arr = Array(repeating: src, count: 2); //!testing!source 
+  sink(sunk : arr[0]); //!testing!sink
+}
+
+// ------- Accessing -------
+
+// subscript already tested above
+
+func test_first() {
+  let src = source();
+  let arr = [src]; //!testing!source
+  sink(sunk : arr.first!); //!testing!sink
+}
+
+// ...
---
+++ tests/ssl/containers/set/test.swift
@@ -0,0 +1,11 @@
+// https://developer.apple.com/documentation/swift/set
+
+func source() -> String {
+    return "I'm bad";
+}
+
+func sink(sunk: String) {
+    print(sunk);
+}
+
+// ...
